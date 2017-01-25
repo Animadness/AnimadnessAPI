9cc1f9ba7cf1cc2619a3780a1331fcde69485d48
Bobby Johnson <popcorn245@gmail.com>
2017-01-24 19:27:25 -0600

    Try that

--- public/wordpress/wp-config.php
+++ public/wordpress/wp-config.php
@@ -1,6 +1,6 @@
 <?php
 require_once(__DIR__ . '/../../vendor/autoload.php');
-if (!class_exists('Dotenv')) {
+if (!class_exists('Dotenv\Dotenv')) {
 	(new \Dotenv\Dotenv(__DIR__.'/../../'))->load();
 }
 
