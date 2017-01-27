84b79524245720601839dab5788b29b9a19a704c
Bobby Johnson <popcorn245@gmail.com>
2017-01-27 00:32:08 -0600

    Try fix for cron

--- app/Post.php
+++ app/Post.php
@@ -10,7 +10,7 @@ class Post extends Corcel
         global $wpdb;
         $dateFormat="Y-m-d H:i:s";
         $now=gmdate($dateFormat);
-        $sql="Select ID from $wpdb->posts where post_status='future' and post_date_gmt<'$now'";
+        $sql="Select ID from ".$wpdb['posts']." where post_status='future' and post_date_gmt<'$now'";
         $resulto = $wpdb->get_results($sql);
         foreach( $resulto as $thisarr ) {
             $changeID=$thisarr->ID;   
